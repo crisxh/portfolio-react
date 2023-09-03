8bd976e86c1f492c4b425f8147ffafde4e002430
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-03 12:53:19 +0200

    added some images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,55 @@
 import './Projects.scss';
 import Project from '../Project/Project';
+import ChatImg from '../../assets/projectImages/Chat With Yourself.png'
+import CheckersImg from '../../assets/projectImages/checkers.png'
+import Magic8BallImg from '../../assets/projectImages/Magic8Ball.png'
+import MarkdownImg from '../../assets/projectImages/Markdown Previewer.png';
+import PomodoroImg from '../../assets/projectImages/Pomodoro.png';
+import RandomQuoteImg from '../../assets/projectImages/RandomQuote.png';
+
+
 
 const projectList = [
     {
+        name: 'Pomodoro Timer',
+        image: PomodoroImg,
+        url: 'https://sprightly-cranachan-c6808e.netlify.app/',
+        tags: ['game',]
+    },
+    {
+        name: "Google Frontend Clone",
+        image: 'https://via.placeholder.com/150?text=Credit Card',
+        url: "https://codepen.io/crisxh/full/PoRjNKQ",
+        tags: ['design']
+    },
+    {
+        name: 'Chat With Yourself',
+        image: ChatImg,
+        url: `https://crisxh.github.io/chat_with_yourself/`,
+        tags: []
+    },
+    {
         name: 'checkers',
-        image: [`checkers.png`],
+        image: CheckersImg,
         url: 'https://crisxh.github.io/checkers',
+        tags: ['game',]
+    },
+    {
+        name: 'Markdown Previewer',
+        image: MarkdownImg,
+        url: 'https://codepen.io/crisxh/full/KKoMbYg',
+        tags: []
+    },
+    {
+        name: 'Random Quote Machine',
+        image: RandomQuoteImg,
+        url: 'https://codepen.io/crisxh/full/NWYrEML',
+        tags: []
+    },
+    {
+        name: "Magic 8 Ball",
+        image: Magic8BallImg,
+        url: '',
         tags: ['game',]
     },
     {
@@ -15,30 +59,10 @@ const projectList = [
         tags: ['game',]
 
     },
-    {
-        name: 'Chat With Yourself',
-        image: 'https://via.placeholder.com/150?text=Chat_With_Yourself',
-        url: `https://crisxh.github.io/chat_with_yourself/`,
-        tags: []
-    },
-    {
-        name: "Magic 8 Ball",
-        image: [`Magic 8 Ball.png`],
-        url: 'https://crisxh.github.io/Magic-8-ball/',
-        tags: ['game',]
-    },
-    {
-        name: 'Random Quote Machine',
-        image: ``,
-        url: 'https://codepen.io/crisxh/full/NWYrEML',
-        tags: []
-    },
-    {
-        name: 'Markdown Previewer',
-        image: 'https://via.placeholder.com/150?text=MarkDown-gPreviewer',
-        url: 'https://codepen.io/crisxh/full/KKoMbYg',
-        tags: []
-    },
+
+
+
+
     {
         name: "Prison Break",
         image: 'https://via.placeholder.com/150?text=Python-PrisonBreak',
@@ -52,12 +76,7 @@ const projectList = [
         url: "https://crisxh.github.io/credit-card-entry",
         tags: ['front-end', 'design', 'frontend-mentor-io']
     },
-    {
-        name: "Google Frontend Clone",
-        image: 'https://via.placeholder.com/150?text=Credit Card',
-        url: "https://codepen.io/crisxh/full/PoRjNKQ",
-        tags: ['design']
-    },
+
     {
         name: "Credit Card Entry Front End",
         image: 'https://via.placeholder.com/150?text=Credit Card',
